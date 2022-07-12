a477bb0514c90676b420c95992bf6a5ea37f28cf
Dingadung <1229jimin@naver.com>
2022-07-12 23:51:38 +0900

    final-dual-priority-queue

--- 2022.07.second-week/DualPriorityQueue/DualPriorityQueue/dualPriorityQueue.cpp
+++ 2022.07.second-week/DualPriorityQueue/DualPriorityQueue/dualPriorityQueue.cpp
@@ -11,48 +11,41 @@ using namespace std;
 int T; // 입력데이터의 수
 int k; // 최대 1000000, Q에 적용할 연산의 개수
 
-struct comp {
-    bool operator()(int a, int b) {
-        return a > b;
-    }
-};
 
-multiset<int> ms;
-priority_queue<int, vector<int>, comp> Q;
 int main() {
     cin >> T;
     while (T--) {
         cin >> k; // 연산 횟수
         char op; // 연산을 나타내는 문자 D 또는 I
         int n;
-        for (int i = 0; i < k; i++) {
+        multiset<int> Q;
+        while(k--) {
             cin >> op >> n;
             if (op == 'I') {
                 // Q에 n 삽입
-                //Q.emplace(n);
-                ms.insert(n);
+                Q.insert(n);
             }
             else if (op == 'D') {
-                if (ms.empty())continue;
+                if (Q.empty())continue;
                 if (n == 1) {
                     // Q에서 최대값 삭제
-                    auto E = ms.end();
+                    auto E = Q.end();
                     E--;
-                    ms.erase(E);
+                    Q.erase(E);
                 }
                 else if (n == -1) {
                     // Q에서 최솟값 삭제
-                    ms.erase(ms.begin());
+                    Q.erase(Q.begin());
                 }
             }
         }
-        if (ms.empty()) {
+        if (Q.empty()) {
             cout << "EMPTY\n";
         }
         else {
-            auto E = ms.end();
+            auto E = Q.end();
             E--;
-            cout << *E << " " << *(ms.begin()) << "\n";
+            cout << *E << " " << *(Q.begin()) << "\n";
         }
     }
     
