6a2a4df2b78d82bcca4d75c6cd2d41ab97dadd09
Dingadung <1229jimin@naver.com>
2022-07-13 10:00:22 +0900

    2022.07.13 final-IOIO-v1

--- 2022.07.second-week/IOIOI/IOIOI/IOIOI.cpp
+++ 2022.07.second-week/IOIOI/IOIOI/IOIOI.cpp
@@ -7,8 +7,36 @@
 #include <string>
 #include <set>
 using namespace std;
+const int N_MAX = 1000001;
+const int M_MAX = 1000001;
+
+long long N, M;
+string S;
+string IO;
+
+int cnt = 0;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N; // O와 I의 개수
+    cin >> M; // 문자열 S의 길이
+    cin >> S;
     
+    for (int i = 0; i < M; i++) {
+        int t = 0;
+        if (S[i] == 'O')continue;
+        while (S[i + 1] == 'O' && S[i + 2] == 'I') {
+            t++;
+            if (t == N) {
+                cnt++;
+                t--; // 중복 방지
+            }
+            i += 2;
+        }
+    }
+    cout << cnt;
     return 0;
 }
