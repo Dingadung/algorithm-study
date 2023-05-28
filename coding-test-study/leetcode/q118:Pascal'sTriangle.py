1ac44f646d6f20e25fe1ce4e3235fb9fc836bfb7
Dingadung <1229jimin@sju.ac.kr>
2023-05-28 13:12:26 +0900

    Study: q118:Pascal'sTriangle

--- coding-test-study/leetcode/q118:Pascal'sTriangle.py
+++ coding-test-study/leetcode/q118:Pascal'sTriangle.py
@@ -1,3 +1,9 @@
+'''
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+'''
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         arr = [[1]]
